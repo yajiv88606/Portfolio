f028b7ce0074b8f92ba0779b2e72b74774fb6898
yajiv88606 <vijay88606@outlook.com>
2021-07-01 13:30:28 +0530

    made a few changes

--- js/theme.js
+++ js/theme.js
@@ -13,20 +13,6 @@ var themeSelector = document.getElementById('theme')
 // --nav-on-hover:white;
 
 
-document.getElementById("theme").addEventListener('click',(event)=>{
-    document.getElementById('theme').innerHTML=''
-    if(dark){
-        variables.style.setProperty('--primary-background','#530e8b')
-        variables.style.setProperty('--secondary-background','#4c1a75')
-        variables.style.setProperty('--primary-header-color','#e7562e')
-        variables.style.setProperty('-secondary-header-color','#21e9db')
-        variables.style.setProperty('--about-details-color','#4acac2')
-        variables.style.setProperty('--nav-color','rgb(216, 216, 216)')
-        variables.style.setProperty('--empty-background-color','#252525;')
-        variables.style.setProperty('--nav-on-hover','white')
-        themeSelector.innerHTML = '<i class="fas fa-lightbulb"></i>'
-        themeSelector.style.setProperty('color','white')   
-    }else{      
 // // default light theme
 // --primary-background: #ffffff;
 // --secondary-background: #f1f1f1;
@@ -37,19 +23,29 @@ document.getElementById("theme").addEventListener('click',(event)=>{
 // --nav-on-hover:rgb(97, 97, 97);
 // --empty-background-color:#rgb(97, 97, 97);
 
+
+document.getElementById("theme").addEventListener('click',(event)=>{
+    if(dark){
+        variables.style.setProperty('--primary-background','#530e8b')
+        variables.style.setProperty('--secondary-background','#4c1a75')
+        variables.style.setProperty('--primary-header-color','#e7562e')
+        variables.style.setProperty('-secondary-header-color','#21e9db')
+        variables.style.setProperty('--about-details-color','#4acac2')
+        variables.style.setProperty('--nav-color','rgb(216, 216, 216)')
+        variables.style.setProperty('--empty-background-color','#252525;')
+        variables.style.setProperty('--nav-on-hover','white')
+        themeSelector.style.setProperty('color','white')   
+    }else{      
         variables.style.setProperty('--primary-background','#ffffff')
         variables.style.setProperty('--secondary-background','#f1f1f1')
         variables.style.setProperty('--primary-header-color','#202020')
         variables.style.setProperty('-secondary-header-color','#2c2a2a')
         variables.style.setProperty('--about-details-color','#181818')
         variables.style.setProperty('--nav-color','rgb(97, 97, 97)')
-        variables.style.setProperty('--nav-on-hover','rgb(97, 97, 97)')
+        variables.style.setProperty('--nav-on-hover','black')
         variables.style.setProperty('--empty-background-color','#rgb(97, 97, 97)')
         variables.style.setProperty('--text-header-color','black')
-        themeSelector.innerHTML = '<i class="fas fa-lightbulb"></i>'
         themeSelector.style.setProperty('color','black')
     }
     dark = !dark;
 })
-
-//1px 2px 1px 0px rgb(255 255 255);
